e560094dbfce642ea45763fac2e9b75453db7bf2
ifsemail9@gmail.com <ifsemail9@gmail.com>
2024-03-13 14:28:03 +0530

    Added bash scripts and updated secret yaml

---
+++ sys-down.sh
@@ -0,0 +1,6 @@
+kubectl delete -f mongo-db.yaml
+kubectl delete -f mongo-express.yaml
+kubectl delete -f mongodb-configmap.yaml
+kubectl delete -f mongo-internal-service.yaml
+kubectl delete -f mongo-secret.yaml
+kubectl delete -f mogo-express-external-service.yaml
