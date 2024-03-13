58e8ab7932d8585522608b8b8b54b6a23475bc03
ifsemail9@gmail.com <ifsemail9@gmail.com>
2024-03-13 21:11:42 +0530

    added shell files for name space confgis

---
+++ with-name-spaces/sys-up.sh
@@ -0,0 +1,14 @@
+kubectl apply -f mongo-secret.yaml
+sleep 5
+kubectl apply -f mongo-db.yaml
+sleep 5
+kubectl apply -f mongo-internal-service.yaml
+sleep 5
+kubectl apply -f mongodb-configmap.yaml
+sleep 5
+kubectl apply -f mongo-express.yaml
+sleep 5
+kubectl apply -f mogo-express-external-service.yaml
+sleep 5
+kubectl get all
+
