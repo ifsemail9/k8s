49b02809b8063a10434d199ad1ff9c4a531dd0b8
ifsemail9 <133851700+ifsemail9@users.noreply.github.com>
2024-03-20 13:31:07 +0530

    Update db-connect.php

--- sample-project/db-connect.php
+++ sample-project/db-connect.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "db-server";
-$username = "user";
-$password = "pass";
+$servername = "db";
+$username = "root";
+$password = "psss";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password);
