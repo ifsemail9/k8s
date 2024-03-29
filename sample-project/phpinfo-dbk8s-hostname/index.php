4ee530ee2298f8268c64639bebf686fe3e5445a1
ifsemail9 <133851700+ifsemail9@users.noreply.github.com>
2024-03-29 12:47:38 +0530

    Update index.php

--- sample-project/phpinfo-dbk8s-hostname/index.php
+++ sample-project/phpinfo-dbk8s-hostname/index.php
@@ -1,8 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Docker Compose Demo</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/dark.min.css">
+</head>
+<body>
+
+    <h1>This is a Docker Compose Demo Page.</h1>
+    <p>This content is being served by an PHP and HTTPD container.</p>
+    <p> Links: php-info.php / index.php </p>
+
+</body>
+</html>
+
 <?php
 
-echo "<br>";
 $hostname = gethostname();
-echo "Hostname : $hostname";
+echo "<br>";
+echo "Hostname: $hostname";
 echo "<br>";
 
 ?>
