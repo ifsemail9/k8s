f002d7da86662dba5763f642209f751f4fcb1142
ifsemail9@gmail.com <ifsemail9@gmail.com>
2024-03-29 09:32:20 +0530

    Adding k8s configured db script

---
+++ sample-project/phpinfo-dbk8s-hostname/db.php
@@ -0,0 +1,16 @@
+<?php
+# 'root' user is blocked from accessing the db from other server other than DB server.
+# $servername = "db";
+$servername = "db-internal-service";
+$username = "test";
+$password = "test";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
---
+++ sample-project/phpinfo-dbk8s-hostname/index.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "<br>";
+$hostname = gethostname();
+echo "Hostname : $hostname";
+echo "<br>";
+
+?>
